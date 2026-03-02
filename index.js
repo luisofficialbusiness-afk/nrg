477033d6eb34532aa57200ca1ac6209d1aa64258
RVEPRTY <luisofficialbusiness@gmail.com>
2026-03-02 16:33:24 -0600

    Update index.js

--- index.js
+++ index.js
@@ -62,3 +62,22 @@ function setupAboutBlank() {
         `);
     });
 }
+
+(function () {
+    const savedTitle = localStorage.getItem("nrg_cloak_title");
+    const savedIcon = localStorage.getItem("nrg_cloak_icon");
+
+    if (savedTitle) {
+        document.title = savedTitle;
+    }
+
+    if (savedIcon) {
+        let link = document.querySelector("link[rel='icon']");
+        if (!link) {
+            link = document.createElement("link");
+            link.rel = "icon";
+            document.head.appendChild(link);
+        }
+        link.href = savedIcon;
+    }
+})();
