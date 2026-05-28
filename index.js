ebf67894509e29d02385fc7055657f07babf3217
RVEPRTY <luisofficialbusiness@gmail.com>
2026-05-28 16:40:37 -0500

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ const __dirname = process.cwd();
 const server = http.createServer();
 const app = express();
 const PORT = 8080;
-const aiRoute = require('./ai-route');
 
 // Authentication Logic
 if (config.challenge !== false) {
@@ -30,8 +29,6 @@ app.use(
     index: "index.html",
   }),
 );
-
-app.use('/', aiRoute);
 
 // 404 Handler
 app.use((req, res) => {
